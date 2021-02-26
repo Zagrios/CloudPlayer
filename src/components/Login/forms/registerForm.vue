b02b949dbe9aeae02e745c70beec3c9ee2e5b784
LastNest <40181755+LastNest@users.noreply.github.com>
2021-02-26 18:32:16 +0100

    add message error

--- src/components/Login/forms/registerForm.vue
+++ src/components/Login/forms/registerForm.vue
@@ -4,10 +4,12 @@
       <div class="input-wrapper">
           <label for="username-input">Nom d'utilisateur</label>
           <input v-model="username" id="username-input" type="text" placeholder="Nom d'utilisateur">
+          <span v-if="usernameError != ''" class="input-form-error">{{usernameError}}</span>
       </div>
       <div class="input-wrapper">
           <label for="email-input">Adresse e-mail</label>
           <input v-model="email" id="email-input" type="email" placeholder="Adresse e-mail">
+          <span v-if="emailError != ''" class="input-form-error">{{emailError}}</span>
       </div>
       <div class="input-wrapper">
           <label for="email-cfirm-input">Confirmez votre adresse e-mail</label>
@@ -16,6 +18,7 @@
       <div class="input-wrapper">
           <label for="password-input">Mot de passe</label>
           <input v-model="password" id="password-input" type="password" placeholder="Mot de passe">
+          <span v-if="passwordError != ''" class="input-form-error">{{passwordError}}</span>
       </div>
       <div class="input-wrapper">
           <label for="password-cfrm-input">Confirmez votre mot de passe</label>
@@ -40,11 +43,24 @@ export default {
             passCfrm: '',
             email:'',
             emailCfrm:'',
+
+            usernameError: '',
+            emailError: '',
+            passwordError: '',
         }
     },
     methods:{
         alreadyRegistred:function() {this.$emit("switch");},
         register:function(){
+            if(this.password != this.passCfrm){this.passwordError = 'Les mots de passes ne correspondent pas.';}else{this.passwordError = ''}
+            if(this.email != this.emailCfrm){this.emailError = 'Les adresses e-mail ne correspondent pas.';}else{this.emailError = ''}
+            if(this.username.length <= 0){this.usernameError = "Veuillez saisir un nom d'utilisateur.";}else{this.usernameError = ''}
+            if(this.email.length <= 0){this.emailError = "Veuillez saisir une adresse e-mail.";}else{this.emailError = ''}
+            if(this.password.length <= 0){this.passwordError = 'Veuillez saisir un mot de passe.'}else{this.passwordError = ''}
+            if(this.passwordError == '' && this.emailError == '' && this.usernameError == ''){this.createUser();}
+            
+        },
+        createUser:function(){
             var data = new FormData();
             data.append('username', this.username);
             data.append('password', this.password);
@@ -62,6 +78,8 @@ export default {
 <style lang="scss">
 
 .form-wrapper{
+
+    
 
     #register-btn{
         margin-top: 35px;
