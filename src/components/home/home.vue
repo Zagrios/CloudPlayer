1a7a4a4000cdbc146312ab3135b37b5ed9734c58
LastNest <40181755+Zagrios@users.noreply.github.com>
2021-03-06 01:55:10 +0100

    add player element

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,31 @@
 <template>
-  <div><h1>OUAI</h1></div>
+  <div id="home">
+    <player></player>
+  </div>
 </template>
 
 <script>
+import player from "./player/player"
+
 export default {
     name: 'Home',
+    components:{
+      player,
+    },
     beforeCreate(){
       if(this.$store.getters.getToken == null || this.$store.getters.getUserName == null){this.$router.push({path:"/"})}
     },
+    
 }
 </script>
 
 <style>
+
+#home{
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  color: white;
+}
 
 </style>
