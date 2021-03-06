5e5271f7b7be310146d8cecb5ad7930c504cef13
LastNest <40181755+Zagrios@users.noreply.github.com>
2021-03-06 01:53:21 +0100

    update css
    This reverts commit 6b5d12e432c0429f7131076b4c96eed65eeedc9e.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
 	<div id="app">
+		<Loader v-if="this.$store.state.isLoad"></Loader>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
+import Loader from "./components/loader"
 
 export default {
 	name: 'App',
+	components:{
+		Loader,
+	},
 	methods:{
 		
 		test:function(){
@@ -25,6 +30,13 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 }
 body {
-  font-family: 'Montserrat',Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'Montserrat',Avenir, Helvetica, Arial, sans-serif;
+}
+
+html, body, #app{
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
 }
 </style>
