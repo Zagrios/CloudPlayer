fbe274d1f639379f863945b4fed0a375f8e72ffa
LastNest <40181755+Zagrios@users.noreply.github.com>
2021-03-13 01:33:26 +0100

    add slidebar

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,21 @@
 <template>
-  <div id="home">
-    <player></player>
-  </div>
+    <div id="home">
+        <div id="content">
+            <nav-bar></nav-bar>
+        </div>
+        <player></player>
+    </div>
 </template>
 
 <script>
 import player from "./player/player"
+import navBar from "./content/navBar"
 
 export default {
     name: 'Home',
     components:{
       player,
+      navBar,
     },
     beforeCreate(){
       if(this.$store.getters.getToken == null || this.$store.getters.getUserName == null){this.$router.push({path:"/"})}
@@ -24,8 +29,18 @@ export default {
 #home{
   width: 100%;
   height: 100%;
+  max-height: 100%;
+  max-width: 100%;
   background-color: black;
   color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+#content{
+    flex: 1;
+    display: flex;
+    flex-direction: row;
 }
 
 </style>
