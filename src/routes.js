db974b71a445d4bf1fa1dba3418af772648546c1
LastNest <40181755+Zagrios@users.noreply.github.com>
2021-03-20 20:36:41 +0100

    add children tracks route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import Init from "./components/init"
 import Home from "./components/home/home"
+import tracks from "./components/home/content/tracks"
 
 export default[
     {path: '/', component: Init},
-    {path: '/home', component: Home}
+    {path: '/home', component: Home, children:[
+        {path: 'tracks', component:tracks}
+    ]}
 ]
