9e54987994f488ec42edc7ca48c388e8b5790374
LastNest <40181755+Zagrios@users.noreply.github.com>
2021-07-03 15:20:27 +0200

    we can now edit username

--- src/components/home/content/profil.vue
+++ src/components/home/content/profil.vue
@@ -24,15 +24,17 @@
 			<div class="input-wrapper hidden" v-bind:class="{close: !usernameHasChange(), open:usernameHasChange()}">
 				<label for="edit-username">Nom d'utilisateur</label>
 				<input v-model="username" id="edit-username" type="text" placeholder="Nom d'utilisateur" />
+				<input id="edit-password" type="password" placeholder="Ancien mot de passe" v-model="verifyPassword"/>
 				<span v-if="usernameError != ''" class="input-form-error">{{}}</span>
 				<div class="btn-wrapper">
-					<span class="btn save">Enregister</span>
+					<span class="btn save" @click="editUsername">Enregister</span>
 					<span class="btn cancel">Annuler</span>
 				</div>
 			</div>
 			<div class="input-wrapper hidden" v-bind:class="{close: !emailHasChange(), open:emailHasChange()}">
 				<label for="edit-email">E-mail</label>
 				<input v-model="email" id="edit-email" type="email" placeholder="Adresse e-mail" />
+				<input id="edit-password" type="password" placeholder="Ancien mot de passe" v-model="verifyPassword"/>
 				<span v-if="usernameError != ''" class="input-form-error">{{}}</span>
 				<div class="btn-wrapper">
 					<span class="btn save">Enregister</span>
@@ -41,9 +43,9 @@
 			</div>
 			<div class="input-wrapper hidden" v-bind:class="{close2: !passwordHasChange(), open2:passwordHasChange()}">
 				<label for="edit-password">Mot de passe</label>
-				<input id="edit-password" type="password" placeholder="Ancien mot de passe" v-model="previousPassword"/>
 				<input id="edit-new-password" type="password" placeholder="Nouveau mot de passe" v-model="newPassword"/>
 				<input id="edit-new-password-cfrm" type="password" placeholder="Comfirmer le nouveau mot de passe" v-model="newPasswordCfrm"/>
+				<input id="edit-password" type="password" placeholder="Ancien mot de passe" v-model="verifyPassword"/>
 				<div class="btn-wrapper">
 					<span class="btn save">Enregister</span>
 					<span class="btn cancel">Annuler</span>
@@ -62,7 +64,7 @@ export default {
 			username:this.$store.getters.getUserName,
 			previousEmail:"",
 			email:"",
-			previousPassword:"",
+			verifyPassword:"",
 			newPassword:"",
 			newPasswordCfrm:"",
 			totalStorage:2.0,
@@ -73,7 +75,7 @@ export default {
 		usernameHasChange:function(){ return !(this.username == this.$store.getters.getUserName); },
 		storagePercent:function(){ return this.usedStorage*100/this.totalStorage; },
 		emailHasChange:function(){ return this.email != this.previousEmail && this.validateEmail(this.email); },
-		passwordHasChange:function(){ return this.validPassword(this.previousPassword) && this.newPassword == this.newPasswordCfrm && this.validPassword(this.newPassword)},
+		passwordHasChange:function(){ return this.newPassword == this.newPasswordCfrm && this.validPassword(this.newPassword)},
 		validPassword:function(pass){ 
 			const re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 			return re.test(pass); 
@@ -90,6 +92,20 @@ export default {
 					this.previousEmail = data.email;
 					this.usedStorage = data.used_storage;
 				}
+			});
+		},
+		editUsername:function(){
+			var data = new FormData();
+			data.append("token", this.$store.getters.getToken);
+			data.append("password", this.verifyPassword);
+			data.append("newUsername", this.username);
+			axios.post("http://localhost/cloudmusic_back/user/actions/editUserProfil.php", data).then((response) => {
+				console.log(response);
+				if(response.status != 200 || response.data == 1){ /* something wrong */ return; }
+				if(response.data == 0){ /* All good */ return; }
+				if(response.data == 2){ /* Wrong password */ return; }
+				if(response.data == 3){ /* username not available */ return; }
+				if(response.data == 4){ /* username invalid */ return; }
 			});
 		}
 	},
@@ -192,9 +208,9 @@ export default {
 		}
 
 	}
-	.close{height: 70px;}
-	.open{height: 100px;}
-	.close2{height: 140px;}
+	.close{height: 62px;}
+	.open{height: 140px;}
+	.close2{height: 100px;}
 	.open2{height: 170px;}
 
 	.account-level{
